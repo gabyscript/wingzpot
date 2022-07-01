361ce0a1592d217b6d71869914656d92fc0f3267
Gabriel <gabrielpreller@gmail.com>
2022-07-01 11:31:58 -0400

    primer commit

---
+++ assets/script/main.js
@@ -0,0 +1,57 @@
+const grupoTarjetas = ["🎷", "🎺", "🥁", "🎸",];
+
+const totalTarjetas = grupoTarjetas.concat(grupoTarjetas).concat(grupoTarjetas);
+
+function barajarTarjetas(){
+    var resultado = totalTarjetas.sort(function(){
+        return 0.5 - Math.random();
+
+    });
+
+    return resultado;
+}
+
+function repartirTarjetas (){
+    let mesa = document.querySelector("#mesa");
+    let tarjetasBarajadas = barajarTarjetas();
+
+    tarjetasBarajadas.forEach(function(elemento) {
+        let tarjeta = document.createElement("div");
+
+        tarjeta.innerHTML = 
+            "<div class= 'tarjeta'> " +
+            "<div class= 'contenido-tarjeta'>" +
+            elemento +
+            "</div>" +
+            "</div>";
+        
+        mesa.appendChild(tarjeta); 
+    });
+
+    
+}
+
+function voltear() {
+    var descubiertas;
+
+    var cartasVolteadas = document.querySelectorAll(".descubierta");
+
+    /*if (cartasVolteadas.length > 1) {
+        return;
+    } */
+    this.classList.add("descubierta");
+
+    descubiertas = document.querySelectorAll(".descubierta");
+
+    
+
+}
+
+repartirTarjetas();
+
+document.querySelectorAll(".tarjeta").forEach(function(elemento) {
+    elemento.addEventListener("click",voltear);
+});
+
+
+
